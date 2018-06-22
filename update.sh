4547ee71b2b17652cb168486de3f8aa3ccc8bdbd
Hao Chen <haochen3611@gmail.com>
2018-06-21 21:24:43 -0400

    Modified the file structure.

--- update.sh
+++ update.sh
@@ -3,24 +3,12 @@
 
 cd /home/hao/Documents/autoCarProject2018/docs/
 
-cd ./rst/
-
-cp ../conf.py .
-
-cp ../index.rst .
-
-cd ..
-
 sphinx-apidoc -f -o ./rst/ ./scripts/
-
-cp index.rst ./rst/
-
-# rm -rf ./html/*
 
 sphinx-build -b html ./rst/ ./html/
 
 sudo rm -rf /var/www/html/test/*
 
-sudo cp -r ./html/* /var/www/html/test/
+sudo cp -r ./_build/html/* /var/www/html/test/
 
 sudo chown -R www-data:www-data /var/www/
